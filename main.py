de8b1e8896d350f148f8352909ec932793a3fef0
Trijai <164654228+Trijai@users.noreply.github.com>
2024-03-27 16:35:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -34,15 +34,19 @@ while True:
                 if fingers[0] == 0 and fingers[1] == 0 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 1:
                     signText = "I was on a call."
                 # If first two fingers are up set 'signText' to "Be Quick!"
-                
+                if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[4] == 0:
+                    signText = "Be Quick!"
                 # If last three fingers are up set 'signText' to "Urgent work!"
-
+                if fingers[0] == 1 and fingers[1] == 0 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 1:
+                    signText = "Urgent work!"
                 # If all fingers and thumb are down set 'signText' to "Stop"
-
+                if fingers[0] == 1 and fingers[1] == 0 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0:
+                    signText = "Stop"
                 cameraFeedImg = cv2.rectangle(
                     cameraFeedImg, (bbox[0]-30, bbox[1]-60), (bbox[0]+bbox[2]+30, bbox[1]-20), (255, 255, 0), -1)
                 # Display the signText at [bbox[0], bbox[1]-30]
-
+                cameraFeedImg = cv2.putText(cameraFeedImg, signText, [
+                    bbox[0], bbox[1]-30], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
     except Exception as e:
         print(e)
 
